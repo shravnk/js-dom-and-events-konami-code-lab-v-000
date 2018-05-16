56a7f866545910fb569e20839597ba0c09946f16
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-16 19:01:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0
 
-
 // const body = document.getElementById('body')
 document.addEventListener('keydown', function (e) {
   const key = parseInt(e.detail || e.which)
