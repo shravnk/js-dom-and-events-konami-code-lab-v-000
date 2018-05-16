cefb94b4bda98f6cf5f077f99cc0c408242c8437
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-16 18:56:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0
-
-const body = document.getElementById('body')
-body.addEventListener('keydown', init)
-
 function init(e) {
   const key = parseInt(e.detail || e.which)
   if (key === code[index]) {
@@ -17,3 +13,7 @@ function init(e) {
     index = 0
   }
 }
+const body = document.getElementById('body')
+body.addEventListener('keydown', init)
+
+
